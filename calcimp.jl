a74d26ef74283857d8323d8ecec464ac0d67fd95
Yoshinobu Ishizaki <ysnbiszk@gmail.com>
2018-09-28 01:30:05 +0900

    calcimp.jl : new

---
+++ calcimp.jl
@@ -0,0 +1,18 @@
+__precompile(false)__
+"""
+calcimp.jl
+Julia version of calcimp.
+"""
+
+version = v"0.1.0"
+
+include("mensur.jl")
+using .Mensur
+
+# command line parser
+
+# execute 
+
+# print output
+
+# end 
